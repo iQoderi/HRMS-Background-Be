8b3d552cce468fd208b39f4c03e3ef6a2421fc32
xzx <453276470@qq.com>
2016-01-10 22:19:43 +0800

    2222222

--- common/models.js
+++ common/models.js
@@ -1,3 +1,11 @@
 /**
  * Created by qoder on 16-1-10.
  */
+module.exports = {
+    user: {
+        username: {type: String, required: true},
+        department:{type:String},
+        tel: {type: String, required: true},
+        mySpareTime: {type: Array, require: true}
+    }
+}
